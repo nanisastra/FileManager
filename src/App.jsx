43d5234adddf38d515607c4449131212b3c23a75
Srinivasa Rao Thota <nani.sastra@gmail.com>
2025-03-16 22:22:36 +0530

    basic layout for login

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,20 @@ function App() {
 
   return (
     <>
-      <h1 className="">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="container mx-auto p-4">
+        <div className="bg-white p-0 rounded-lg shadow-lg flex h-[90vh] w-full">
+          <div className="p-4 w-1/2">
+            <div className="flex items-center justify-center h-screen">
+              <form className="bg-white p-8">
+                <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+              </form>
+            </div>
+          </div>
+          <div className="text-white p-4 w-1/2 bg-indigo-800 rounded-r-lg">
+            test
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
